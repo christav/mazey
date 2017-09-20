aba8c2374d33789626dabf9cee909fdb0d6ba55d
Chris Tavares <ctavares@microsoft.com>
2017-09-19 23:58:16 -0700

    Using unicode character to print the maze.
    Looks a lot nicer than straight ascii.

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import sys
 import random
 from maze_maker import make_maze
-from printer import print_maze
+from printer import MazePrinter, ascii_char_set, unicode_char_set
 from solver import solve
 
 def run(rows, cols):
     m = make_maze(rows, cols)
     solve(m)
-    print_maze(sys.stdout, m)
+    printer = MazePrinter(char_set = unicode_char_set)
+    printer.print_maze(m)
 
 if __name__ == "__main__":
     rows = int(sys.argv[1]) if len(sys.argv) >= 2 else 10
